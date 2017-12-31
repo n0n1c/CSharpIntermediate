763293efc3d8588f317745f3fa151374b0ca7dfe
Nong <nong.suon@gmail.com>
2017-12-30 22:47:27 -0500

    finished C Sharp intermediate lecture 7

---
+++ CShapIntermediate.Customer/CShapIntermediate.Customer/Order.cs
@@ -0,0 +1,7 @@
+﻿namespace CShapIntermediate.Customer
+{
+    public class Order
+    {
+        
+    }
+}
---
+++ CShapIntermediate.Customer/CShapIntermediate.Customer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShapIntermediate.Customer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+
+        }
+    }
+}
