fe396ab3a62a637f2b6b622f096393924df82531
Nong <nong.suon@gmail.com>
2018-01-01 13:04:29 -0500

    finished lecture 10 C # intermediate

--- Methods/Methods/Point.cs
+++ Methods/Methods/Point.cs
@@ -1,4 +1,6 @@
-﻿namespace Methods
+﻿using System;
+
+namespace Methods
 {
     public class Point
     {
@@ -19,8 +21,10 @@
 
         public void Move(Point newLocation)
         {
-            this.X = newLocation.X;
-            this.Y = newLocation.Y;
+            if (newLocation == null)
+                throw new ArgumentNullException("newLocation");
+
+            Move(newLocation.X, newLocation.Y);
         }
     }
 }
