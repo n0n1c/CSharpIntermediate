f6dccc609d795d21697a2b78d1310bd06b9a3291
Nong <nong.suon@gmail.com>
2018-01-02 01:04:31 -0500

    finished lecture 13 c# intermediate

--- CSharpIntermediate.Properties/CSharpIntermediate.Properties/Program.cs
+++ CSharpIntermediate.Properties/CSharpIntermediate.Properties/Program.cs
@@ -10,8 +10,7 @@ namespace CSharpIntermediate.Properties
     {
         static void Main(string[] args)
         {
-            var person = new Person();
-            person.Birthdate = new DateTime(1978, 1, 1);
+            var person = new Person(new DateTime(1978, 1, 1));
             Console.WriteLine(person.Age);
         }
     }
