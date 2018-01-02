dd73075e0bccbb3c00468af79c600ba728d89aea
Nong <nong.suon@gmail.com>
2018-01-02 00:49:44 -0500

    finished lecture 12 c# intermediate

---
+++ CSharpIntermediate.Properties/CSharpIntermediate.Properties/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharpIntermediate.Properties
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public DateTime Birthdate { get; private set; }
+
+        public Person(DateTime birthdate)
+        {
+            Birthdate = Birthdate;
+        }
+
+       
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+        }
+    }
+}
---
+++ CSharpIntermediate.Properties/CSharpIntermediate.Properties/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediate.Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Birthdate = new DateTime(1978, 1, 1);
+            Console.WriteLine(person.Age);
+        }
+    }
+}
