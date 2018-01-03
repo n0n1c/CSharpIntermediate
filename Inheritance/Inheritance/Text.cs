531f62cfa8730006c09a9b304702ab6a138b73d2
Nong <nong.suon@gmail.com>
2018-01-03 15:27:49 -0500

    finished lecture 18 C# intermediate

---
+++ Inheritance/Inheritance/Text.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class Text : PresentationObject
+    {
+        public int Fontsize { get; set; }
+        public string Fontname { get; set; }
+
+        public void AddHyperLink(string url)
+        {
+            Console.WriteLine("We added a link to " + url);
+        }
+
+    }
+}
