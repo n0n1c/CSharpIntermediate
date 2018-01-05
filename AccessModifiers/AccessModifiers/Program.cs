6eec04daf2a037ba2f8753b096d3aa3820d611ec
Nong <nong.suon@gmail.com>
2018-01-04 20:15:44 -0500

    finished lecture 21 C# intermediate

---
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+
+namespace AccessModifiers
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            Amazon.RateCalculator calculator = new RateCalculator();
+        }
+    }
+}
