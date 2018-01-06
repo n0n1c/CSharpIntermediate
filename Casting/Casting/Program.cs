cbfead004945423bad87b93f01f665171511de56
Nong <nong.suon@gmail.com>
2018-01-06 14:57:53 -0500

    finished lecture 23 C# intermediate

---
+++ Casting/Casting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = new Text();
+            Text text = (Text) shape;
+        }
+    }
+}
