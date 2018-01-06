88a9f11ae116a93b271ef21b5199a472ea9962ae
Nong <nong.suon@gmail.com>
2018-01-06 15:10:37 -0500

    finished lecture 24 C# intermediate

---
+++ Boxing/Boxing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("Nong");
+            list.Add(DateTime.Today);
+
+            var number = (int) list[1];
+
+            var anotherList = new List<int>();
+
+            var names = new List<string>();
+
+            names.Add();
+        }
+    }
+}
